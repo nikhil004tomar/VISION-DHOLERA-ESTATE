b46d9671ba24ffd6f5692896576ce49c0b481df4
nikhil004tomar <tomarthakurnikhil973@gmail.com>
2026-08-18 00:32:54 -0700

    Fix production CORS configuration

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -65,8 +65,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
+    "https://visiondholera.com",
+    "https://www.visiondholera.com",
+    "https://api.visiondholera.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
